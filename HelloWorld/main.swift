d4c6ff8f6356ccb63e52dd2f8357a1ef30d4ec82
Tutanck <tutanck@gmail.com>
2017-07-06 10:29:04 +0200

    Initial Commit

---
+++ HelloWorld/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloWorld
+//
+//  Created by Zenika on 06/07/2017.
+//  Copyright © 2017 DevArtisant. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
